b93734be2a36aca2a59fcdf7322b3ba30f3dc708
FrenkenFlores <saifualdin.evloev@gmail.com>
2024-03-20 12:14:22 +0300

    update #7: add ship
    Signed-off-by: FrenkenFlores <saifualdin.evloev@gmail.com>

--- main.py
+++ main.py
@@ -8,27 +8,6 @@ FPS = 60
 WIDTH = 800
 HEIGHT = 600
 
-
-def update_background():
-    space_surface = pygame.Surface((WIDTH, HEIGHT))
-    space_surface.fill(color=(25, 15, 35))
-    stars_surface = [pygame.image.load("graphics/star_0.png").convert_alpha()] * 10 + [
-        pygame.image.load("graphics/star_1.png").convert_alpha(),
-        pygame.image.load("graphics/star_2.png").convert_alpha()
-    ]
-    stars_number = 30
-    stars_density = 1
-    screen.blit(space_surface, (0, 0))
-    for n in range(stars_number):
-        screen.blit(
-            random.choice(stars_surface),
-            (
-                random.randrange(0, WIDTH, stars_density),
-                random.randrange(0, HEIGHT, stars_density)
-            )
-        )
-
-
 def earth_animation():
     while True:
         for _, _, files in os.walk('graphics/earth'):
@@ -37,16 +16,35 @@ def earth_animation():
 
 
 def main():
-    random.seed = 1
+    random.seed = 42
     clock = pygame.time.Clock()
     tmp_time = time.time()
     # Create a regular surface.
-    update_background()
+    space_surface = pygame.Surface((WIDTH, HEIGHT))
+    space_surface.fill(color=(25, 15, 35))
+    screen.blit(space_surface, (0, 0))
+    stars_surface = [pygame.image.load("graphics/star_0.png").convert_alpha()] * 10 + [
+        pygame.image.load("graphics/star_1.png").convert_alpha(),
+        pygame.image.load("graphics/star_2.png").convert_alpha()
+    ]
+    stars_number = 30
+    stars_density = 1
+    for n in range(stars_number):
+        screen.blit(
+            random.choice(stars_surface),
+            (
+                random.randrange(0, WIDTH, stars_density),
+                random.randrange(0, HEIGHT, stars_density)
+            )
+        )
     earth_surfaces = earth_animation()
     earth_surface = next(earth_surfaces)
     title_text = pygame.font.Font("fonts/Pixeltype.ttf", 50)
     title_text_surface = title_text.render('E-Defender', False, (255, 255, 255))
-
+    # ship_surface = pygame.image.load("graphics/ship.png")
+    # ship_rec = ship_surface.get_rect(midbottom=(33, 33))
+    ships_num = 1
+    ships_cords = [(random.randrange(0, WIDTH), random.randrange(0, HEIGHT)) for _ in range(ships_num)]
     while True:
         # Add exit code.
         for event in pygame.event.get():
@@ -54,10 +52,18 @@ def main():
                 pygame.quit()
                 sys.exit(0)
         # Draw objects.
-        if time.time() - tmp_time > 3:
+        if time.time() - tmp_time > 1:
             # Save time.
             tmp_time = time.time()
-            update_background()
+            screen.blit(space_surface, (0, 0))
+            for n in range(stars_number):
+                screen.blit(
+                    random.choice(stars_surface),
+                    (
+                        random.randrange(0, WIDTH, stars_density),
+                        random.randrange(0, HEIGHT, stars_density)
+                    )
+                )
             earth_surface = next(earth_surfaces)
         # BLIT - Block Image Transfer - put one surface on another surface.
         screen.blit(earth_surface, (
@@ -68,6 +74,11 @@ def main():
                 (HEIGHT - title_text_surface.get_height() - earth_surface.get_height() - 100) / 2
             )
         )
+        for i in range(len(ships_cords)):
+            ship_surface = pygame.image.load("graphics/ship.png").convert_alpha()
+            pygame.transform.rotate(ship_surface, 44)
+            screen.blit(ship_surface, ships_cords[i])
+            ships_cords[i] = (ships_cords[i][0] + 1, ships_cords[i][1] + 1)
         # Update the main screen.
         pygame.display.update()
         # Should not run faster than FPS frames peer second.
